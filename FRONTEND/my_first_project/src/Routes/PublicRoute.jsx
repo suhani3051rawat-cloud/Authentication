4c129d19ca68d11e62bd3dfaab691e5c794b104d
suhani <suhani3051rawat@gmail.com>
2026-07-15 16:15:49 +0530

    Updated changes

--- FRONTEND/my_first_project/src/Routes/PublicRoute.jsx
+++ FRONTEND/my_first_project/src/Routes/PublicRoute.jsx
@@ -6,21 +6,19 @@ import { Navigate, Outlet } from 'react-router-dom';
 
 export const PublicRoute = ()=>{
  const[isloggedin , setIsLoggedIn] = useState(null);
- console.log(isloggedin);
  useEffect(()=>{
   const fetchLogin = async ()=>{
     try {
      let resp = await axios.get('http://localhost:4000/api/auth/user/me',{
         withCredentials : true
      })
-     console.log(resp.data.message);
      if(resp.data.message == 'User logged in'){
         setIsLoggedIn(true);
      }
      else{
       setIsLoggedIn(false);
      }
-    } catch (error) {
+    } catch (error){
        console.log("error : ", error); 
        setIsLoggedIn(false)
     }
